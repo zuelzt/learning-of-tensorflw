c0a79f2602b3073f3a152ea0d359240d3e4edb7c
zjwzt1123 <33337293+zuelzt@users.noreply.github.com>
2018-03-30 17:01:59 +0800

    Add files via upload

---
+++ eager_api.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 30 16:57:24 2018
+
+@author: Rorschach
+@mail: 188581221@qq.com
+"""
+import warnings
+warnings.filterwarnings('ignore')
+
+
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
